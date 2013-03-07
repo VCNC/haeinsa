9f0fce9726c5f4b1c6c1d6c98b1794ca02b0f579
Myungbo Kim <mbkim@vcnc.co.kr>
2013-03-07 15:03:35 +0900

    fixed bug of HaeinsaColumnTracker

--- src/main/java/kr/co/vcnc/haeinsa/HaeinsaColumnTracker.java
+++ src/main/java/kr/co/vcnc/haeinsa/HaeinsaColumnTracker.java
@@ -9,7 +9,15 @@ import org.apache.hadoop.hbase.util.Bytes;
 
 import com.google.common.collect.Sets;
 
+/**
+ * Tracking parameters of {@link HaeinsaScan} and {@link HaeinsaIntraScan} inside {@link HaeinsaTable}.
+ * <p>Tracker can determine whether specific HaeinsaKeyValue satisfy the scan parameters.
+ * 
+ * @author Myungbo Kim
+ *
+ */
 public class HaeinsaColumnTracker {
+	//	{ family -> qualifier } 
 	private final Map<byte[], NavigableSet<byte[]>> familyMap = new TreeMap<byte[], NavigableSet<byte[]>>(
 			Bytes.BYTES_COMPARATOR);
 
@@ -18,6 +26,18 @@ public class HaeinsaColumnTracker {
 	private final byte[] maxColumn;
 	private final boolean maxColumnInclusive;
 
+	/**
+	 * Constructor of HaeinsaColumnTracker. 
+	 * 
+	 * <p>If this ColumnTracker track {@link HaeinsaScan}, minColumn, maxColumn should be null and 
+	 * minColumnInclusive, maxColumnInclusive should be false. 
+	 * @param familyMap
+	 * 					
+	 * @param minColumn
+	 * @param minColumnInclusive
+	 * @param maxColumn
+	 * @param maxColumnInclusive
+	 */
 	public HaeinsaColumnTracker(Map<byte[], NavigableSet<byte[]>> familyMap,
 			byte[] minColumn, boolean minColumnInclusive, byte[] maxColumn,
 			boolean maxColumnInclusive) {
@@ -27,16 +47,22 @@ public class HaeinsaColumnTracker {
 		this.maxColumnInclusive = maxColumnInclusive;
 		for (Entry<byte[], NavigableSet<byte[]>> entry : familyMap.entrySet()) {
 			if (entry.getValue() != null) {
-				NavigableSet<byte[]> set = Sets
+				NavigableSet<byte[]> qualifierSet = Sets
 						.newTreeSet(Bytes.BYTES_COMPARATOR);
-				set.addAll(entry.getValue());
-				this.familyMap.put(entry.getKey(), set);
+				qualifierSet.addAll(entry.getValue());
+				this.familyMap.put(entry.getKey(), qualifierSet);
 			} else {
 				this.familyMap.put(entry.getKey(), null);
 			}
 		}
 	}
 
+	/**
+	 * Return true if qualifier of kv is placed between minColumn and maxColumn.
+	 * <p>Using lexicographical ordering to compare byte[]
+	 * @param kv
+	 * @return
+	 */
 	public boolean isColumnInclusive(HaeinsaKeyValue kv) {
 		int cmpMin = 1;
 		if (this.minColumn != null){
@@ -51,6 +77,7 @@ public class HaeinsaColumnTracker {
 			return false;
 		}
 		
+		//	kv 의 column 이 minColumn 값이 정하는 requirement 를 만족시키고 있으면서 maxColumn requirement 는 없을 때
 		if (this.maxColumn == null){
 			return true;
 		}
@@ -65,18 +92,38 @@ public class HaeinsaColumnTracker {
 		return false;
 	}
 
+	/**
+	 * Argument 로 넘긴 HaeinsaKeyValue 가 scan 범위 안에 포함되는 지 판별해 준다. 다음 세 가지 경우가 있을 수 있다.
+	 * 
+	 * <p>1. Scan 에 family 가 전혀 지정되지 않은 경우 - qualifier 범위에만 포함되면 된다.
+	 * 
+	 * <p>2. Scan 에 family 가 지정되어 있고, kv 의 family 와 같지만 qualifier 는 지정되어 있지 않은 경우 - kv 와 family 가 같고 
+	 * qualifier 가 Scan 범위 안에 포함되면 된다.
+	 * 
+	 * <p>3. Scan 에 family 와 qualifier 가 함께 지정되어 있는 경우 - kv 의 (family, qualifier) 가 같아야 한다.
+	 * @param kv
+	 * @return
+	 */
 	public boolean isMatched(HaeinsaKeyValue kv) {
+		//	familyMap 이 비어있다는 것은 scan 시에 특정 family 를 지정하지 않고 모든 (family, qualifier)를 scan 하는 것이라고 가정하여,
+		//	isColumnInclusive(kv) 를 바로 호출한다.
+		//	{ empty }
 		if (familyMap.isEmpty()) {
 			return isColumnInclusive(kv);
 		}
+		
 		NavigableSet<byte[]> set = familyMap.get(kv.getFamily());
-		if (set == null) {
+		//	해당 family 는 설정 되어 있고, qualifier 는 설정되어 있지 않은 경우
+		//	{ family -> null }
+		if (familyMap.containsKey(kv.getFamily()) && set == null) {
 			return isColumnInclusive(kv);
 		}
+		
+		//	해당 family 가 familyMap 에 있고 qualifier 도 설정되어 있는 경우
+		//	{ family -> qualifier }
 		if (set.contains(kv.getQualifier())) {
 			return isColumnInclusive(kv);
 		}
-
 		return false;
 	}
 }
