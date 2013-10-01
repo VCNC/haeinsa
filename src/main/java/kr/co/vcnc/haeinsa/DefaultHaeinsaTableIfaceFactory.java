34c59f47da6af6e0bd86eb4c6ba3063140a76b06
James Lee <james@vcnc.co.kr>
2013-10-01 16:02:27 +0900

    HAEINSA-68 update owner format of license header of all source codes

--- src/main/java/kr/co/vcnc/haeinsa/DefaultHaeinsaTableIfaceFactory.java
+++ src/main/java/kr/co/vcnc/haeinsa/DefaultHaeinsaTableIfaceFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013 VCNC, inc
+ * Copyright (C) 2013 VCNC Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
