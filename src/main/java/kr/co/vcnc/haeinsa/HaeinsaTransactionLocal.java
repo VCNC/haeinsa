21e438c7cf3b365481df6adc9e042587f261f206
Ted Kim <ted@vcnc.co.kr>
2014-04-16 11:44:51 +0900

    Add javadoc

--- src/main/java/kr/co/vcnc/haeinsa/HaeinsaTransactionLocal.java
+++ src/main/java/kr/co/vcnc/haeinsa/HaeinsaTransactionLocal.java
@@ -22,29 +22,64 @@ import javax.annotation.Nullable;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
 
-public class HaeinsaTransactionLocal<T> {
+/**
+ * A HaeinsaTransactionLocal stores values associated with {@link HaeinsaTransaction}.
+ * <p>
+ * You can create transaction local using {@link HaeinsaTransactionLocal#newLocal()}.
+ */
+public final class HaeinsaTransactionLocal<T> {
+    private HaeinsaTransactionLocal() {}
 
+    /*
+     * Creates a transaction local variable.
+     * @return a new transaction local variable
+     */
     public static <T> HaeinsaTransactionLocal<T> newLocal() {
         return new HaeinsaTransactionLocal<T>();
     }
 
+    /*
+     * Returns the value associated with the specified transaction.
+     * If no value is set, returns <code>null</code>.
+     * @param tx the transaction for which the variable it to be retrieved
+     * @return the specified transaction's value of this transaction local,
+     * or <code>null</code> if no value is set
+     */
     @Nullable
     public T get(HaeinsaTransaction tx) {
         Preconditions.checkNotNull(tx);
         return (T) tx.getLocals().get(this);
     }
 
+    /*
+     * Sets the value associated with the specified transaction.
+     * Storing <code>null</code> as a value is not allowed.
+     * @param tx the the transaction for which the value will be set
+     * @param value the value to be associated with the transaction (cannot be <code>null</code>)
+     */
     public void set(HaeinsaTransaction tx, T value) {
         Preconditions.checkNotNull(tx);
         Preconditions.checkNotNull(value);
         tx.getLocals().set(this, value);
     }
 
+    /*
+     * Returns <code>true</code> if this transaction local variable has the value associated with
+     * the specified transaction.
+     * @param tx the transaction for which the variable it to be retrieved
+     * @return <code>true</code> if this transaction local variable has the value associated with
+     * the specified transaction
+     */
     public boolean isSet(HaeinsaTransaction tx) {
         Preconditions.checkNotNull(tx);
         return tx.getLocals().isSet(this);
     }
 
+    /*
+     * Removes the value associated with the specified transaction.
+     * @param tx the the transaction for which the value will be removed
+     * @return the previous value associated with transaction, or null if there was no value
+     */
     public T remove(HaeinsaTransaction tx) {
         Preconditions.checkNotNull(tx);
         return (T) tx.getLocals().remove(this);
